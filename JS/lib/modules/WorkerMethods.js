4eda30657a26ccd0c9b6ff6c4228388bf9fd3692
PatrykMaternicki <patryk.maternicki@gmail.com>
2016-10-20 08:06:32 +0200

    delete old ver

--- JS/lib/modules/WorkerMethods.js
+++
@@ -1,8 +0,0 @@
-var Methods = function (){
-   this.field  = 2; 
-}
-
-module.exports = Methods;
-Methods.prototype.summaryDaySalary = function (stockSalaryDay, salary){
-    return stockSalaryDay * salary;
-}
